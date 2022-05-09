1ca909b329aef1256cfea98a001a1119bef65eb5
cgrfaria <50423861+cgrfaria@users.noreply.github.com>
2022-05-09 07:30:37 -0300

    mobile first

---
+++ main.js
@@ -0,0 +1,46 @@
+window.addEventListener('scroll', onScroll);
+
+onScroll();
+function onScroll() {
+  showNavOnScroll();
+  showBackToTopButtonOnScroll();
+}
+
+function showNavOnScroll() {
+  if (scrollY > 10) {
+    navigation.classList.add('scroll');
+  } else {
+    navigation.classList.remove('scroll');
+  }
+}
+
+function showBackToTopButtonOnScroll() {
+  if (scrollY > 400) {
+    backToTopButton.classList.add('show');
+  } else {
+    backToTopButton.classList.remove('show');
+  }
+}
+
+function openMenu() {
+  document.body.classList.add('menu-expanded');
+}
+
+function closeMenu() {
+  document.body.classList.remove('menu-expanded');
+}
+
+ScrollReveal({
+  origin: 'top',
+  distance: '50px',
+  duration: 900,
+}).reveal(`
+#home, 
+#home img, 
+#home .stats, 
+#services,
+#services header,
+#services .card,
+#about,
+#about header,
+#about .content`);
